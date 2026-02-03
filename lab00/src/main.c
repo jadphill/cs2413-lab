7d64fa0390f0e33cf3c71310b032ad0cc76b59ff
jadphill <jadphill@ttu.edu>
2026-02-03 08:49:37 -0600

    Update main.c

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,11 +4,12 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: Shawn\n");
-    printf("R#: 11714613\n");
-    printf("Section: 002\n");
+    printf("Name: Jaden\n");
+    printf("R#: 11945150\n");
+    printf("Section: 504\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
 
     return 0;
 
 }
+
